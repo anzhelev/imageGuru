2b70bfaebaa8d2a19a49ebbfd221b0dfe0bf68e9
Andrey Zhelev <anzhelev@gmail.com>
2024-02-28 14:33:06 +0300

    задача Спринт 9

--- imageGuru/ImagesList/SingleImageViewController.swift
+++ imageGuru/ImagesList/SingleImageViewController.swift
@@ -34,7 +34,7 @@ final class SingleImageViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         scrollView.minimumZoomScale = 0.1
-        scrollView.maximumZoomScale = 10.25
+        scrollView.maximumZoomScale = 1.25
         if let image {
             imageView.image = image
             imageView.frame.size = image.size
@@ -65,7 +65,7 @@ final class SingleImageViewController: UIViewController {
         let imageSize = image.size
         let hScale = visibleRectSize.width / imageSize.width
         let vScale = visibleRectSize.height / imageSize.height
-        let scale = min(maxZoomScale, max(minZoomScale, min(hScale, vScale)))
+        let scale = min(maxZoomScale, max(minZoomScale, max(hScale, vScale)))
         scrollView.setZoomScale(scale, animated: false)
         scrollView.layoutIfNeeded()
         
@@ -73,7 +73,6 @@ final class SingleImageViewController: UIViewController {
         let x = (newContentSize.width - visibleRectSize.width) / 2
         let y = (newContentSize.height - visibleRectSize.height) / 2
         scrollView.setContentOffset(CGPoint(x: x, y: y), animated: false)
-        //        print (visibleRectSize, imageSize, scale, newContentSize, x, y)
     }
 }
 
