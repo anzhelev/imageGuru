17c805527ca767fb46a69811be0407f019ba029b
Andrey Zhelev <anzhelev@gmail.com>
2024-03-07 13:08:46 +0300

    добавлены константы

---
+++ imageGuru/Helpers/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  imageGuru
+//
+//  Created by Andrey Zhelev on 02.03.2024.
+//
+import Foundation
+
+enum Constants {
+    static let AccessKey = "7X0Xb-7BJ4O97VjzUIQUxum5eXDYVM65-nd9mEMKmew"
+    static let SecretKey = "5YRMeSBi9_ABw2iv_uBqRvAQ_xIKIxF2228UtGWAhTU"
+    static let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    static let AccessScope = "public+read_user+write_likes"
+    static let DefaultBaseURL = URL(string: "https://api.unsplash.com")!
+}
