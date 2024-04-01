34207e84309c9e7cca887d6dc2414d5c34d567bf
Andrey Zhelev <anzhelev@gmail.com>
2024-04-02 01:54:16 +0300

    спринт 11 тема 6 урок 1 (до)

--- imageGuru/Auth/OAuth2TokenStorage.swift
+++ imageGuru/Auth/OAuth2TokenStorage.swift
@@ -5,11 +5,11 @@
 //  Created by Andrey Zhelev on 13.03.2024.
 //
 import Foundation
+import SwiftKeychainWrapper
 
 final class OAuth2TokenStorage {
     
     // MARK: - Private Properties
-    private let userDefaults = UserDefaults.standard
     private enum Keys: String {
         case token
     }
@@ -17,10 +17,12 @@ final class OAuth2TokenStorage {
     // MARK: - Computed Properties
     var token: String? {
         get {
-            return userDefaults.string(forKey: Keys.token.rawValue)
+            return KeychainWrapper.standard.string(forKey: Keys.token.rawValue)
         }
         set {
-            userDefaults.set(newValue, forKey: Keys.token.rawValue)
+            if let token = newValue {
+                KeychainWrapper.standard.set(token, forKey: Keys.token.rawValue)
+            }
         }
     }
 }
